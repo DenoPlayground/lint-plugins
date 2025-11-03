9bd64f0e8840d460a4ed3a2e0c97ba45c1d37bca
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-11-03 18:32:09 +0100

    Update colon_spacing.ts

--- src/colon_spacing.ts
+++ src/colon_spacing.ts
@@ -1,91 +1,55 @@
 /**
  * This plugin ensures consistent spacing before and after the colon for type definitions.
  */
-const plugin : Deno.lint.Plugin = {
-  name: "colon-spacing",
+export default {
+  name: 'colon-spacing',
   rules: {
-    "no-space-before-color": {
-      create(context) {
+    'after-function': {
+      create(context): Deno.lint.LintVisitor {
         return {
-          Property(node) {
-            if (!node.shorthand && !node.method) {
-              const nodeText = context.sourceCode.getText(node)
-              const colonText = nodeText.slice(
-                node.key.range[1]-node.key.range[0],
-                -(node.value.range[1] - node.value.range[0])
-              );
-              if (colonText !== ': ') {
-                const beforeColonText = nodeText.slice(
-                  0,
-                  node.key.range[1]-node.key.range[0]
-                );
-                const afterColonText = nodeText.slice(
-                  -(node.value.range[1] - node.value.range[0])
-                );
-                context.report({
-                  node,
-                  message: `Wrong spacing around colon.`,
-                  fix(fixer) {
-                    return fixer.replaceText(node, beforeColonText + colonText.replace(/ *: */, ': ') + afterColonText)
-                  }
-                })
-              }
-             
-            }
-          },
-          TSPropertySignature(node) {
-            if ((node.typeAnnotation?.range[0] ?? node.key.range[1]) - node.key.range[1] !== 1) {
-              context.report({
-                node,
-                message: `Wrong spacing before colon.`,
-                fix(fixer) {
-                  return fixer.replaceTextRange(
-                    [node.key.range[1], (node.typeAnnotation?.range[0] ?? node.key.range[1])],
-                    ' '
-                  )
-                }
-              })
-            }
-          },
-          Identifier(node) {
-            if (node.typeAnnotation) {
-              const spaceAfter = node.typeAnnotation.range[0] - (node.range[0] + node.name.length)
+          TSTypeAnnotation(node): void {
+            if (node.parent.type === 'FunctionDeclaration') {
+              const functionStart = node.parent.range[0];
+              const sectionEnd = node.range[0];
+              const index =
+                context.sourceCode.getText(node.parent).substring(0, sectionEnd - functionStart).search(/\) *$/) + 1;
+              const sectionStart = functionStart + index;
 
-              if (spaceAfter !== 1) {
+              if (index !== -1 && sectionEnd - sectionStart !== 1) {
                 context.report({
-                  node,
-                  message: `Wrong spacing before colon.`,
-                  range: [node.range[0], node.range[0] + spaceAfter],
-                  fix(fixer) {
-                    return fixer.replaceTextRange([(node.range[0] + node.name.length), (node.typeAnnotation!.range[0])], ' ')
+                  message: `Wrong colon spacing. Expected 1 space after function.`,
+                  range: [sectionStart - 1, sectionEnd + 1],
+                  fix(fixer) : Deno.lint.Fix{
+                    return fixer.replaceTextRange([sectionStart, sectionEnd], ' ');
                   }
-                })
+                });
               }
             }
           }
-        }
+        };
       }
     },
-    "no-space-after-color": {
-      create(context) {
+    'before-type': {
+      create(context): Deno.lint.LintVisitor {
         return {
-          TSTypeAnnotation(node) {
-            
-            const spaceInFront = node.typeAnnotation.range[0] - (node.range[0] + 1)
-            
-            if (spaceInFront !== 1) {
-              context.report({
-                node,
-                message: `Wrong spacing after colon.`,
-                fix(fixer) {
-                  return fixer.replaceTextRange([node.range[0] + 1, node.range[0] + 1 + spaceInFront], ' ')
-                }
-              });
+          TSTypeAnnotation(node): void {
+            if (node.parent.type === 'FunctionDeclaration') {
+              const sectionStart = node.range[0] + 1;
+              const sectionEnd = node.typeAnnotation.range[0];
+
+              if (sectionEnd - sectionStart !== 1) {
+                context.report({
+                  message: `Wrong colon spacing. Expected 1 space before type.`,
+                  range: [sectionStart - 1, sectionEnd + 1],
+                  fix(fixer) : Deno.lint.Fix{
+                    return fixer.replaceTextRange([sectionStart, sectionEnd], ' ');
+                  }
+                });
+              }
             }
           }
-        }
+        };
       }
     }
   }
-}
-export default plugin;
+} satisfies Deno.lint.Plugin;
