5f69eefc4b2736058f5bb525d16407b4f42fbad1
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-11-04 16:15:25 +0100

    feat: import formatting

---
+++ src/plugins/imports.ts
@@ -0,0 +1,30 @@
+/**
+ * This plugin ensures consistent formatting for imports.
+ */
+const imports : Deno.lint.Plugin = {
+  name: 'imports',
+  rules: {
+    'spacing': {
+      create(context) : Deno.lint.LintVisitor {
+        return {
+          ImportDeclaration(node) : void {
+            const sectionStart = node.range[0];
+            const sectionEnd = node.range[1];
+
+            if (node.specifiers.length > 0) {
+              context.report({
+                message: `Wrong colon spacing. Expected 1 space before colon.`,
+                range: [sectionStart - 1, sectionEnd + 1],
+                // fix(fixer) : Deno.lint.Fix{
+                //   return fixer.replaceTextRange([sectionStart, sectionEnd], ' ');
+                // }
+              });
+            }
+          }
+        };
+      }
+    }
+  }
+};
+
+export default imports;
