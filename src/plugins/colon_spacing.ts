8b01a5262e783c5c0af135d585d32e486780ec99
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-11-06 17:07:34 +0100

    fix: Spacing after colon

--- src/plugins/colon_spacing.ts
+++ src/plugins/colon_spacing.ts
@@ -2,6 +2,7 @@ import { rangeDistance } from '../range_distance.ts';
 import { rangePadding } from '../range_padding.ts';
 
 const spaceBeforeColon = 1;
+const spaceAfterColon = 1;
 
 /**
  * This plugin ensures consistent spacing before and after the colon for type definitions.
@@ -12,6 +13,51 @@ const colonSpacing : Deno.lint.Plugin = {
     'before-colon': {
       create(context) : Deno.lint.LintVisitor {
         return {
+          FunctionExpression(node) : void {
+            if (node.returnType) {
+              // Section | from "<id> |(<...params>) |:<type>"
+              const section : Deno.lint.Range = [
+                node.parent.type === 'Property' ? node.parent.key.range[1] :
+                node.parent.type === 'CallExpression' ? node.parent.callee.range[1] + 1 : node.parent.range[1],
+                node.returnType.range[0]
+              ]
+
+              if (node.params.length > 0) {
+                const last = node.params[node.params.length - 1]
+
+                if (last.type === 'Identifier') {
+                  // Section | from "<id>(<...params>|)  |:<type>"
+                  section[0] = last.range[1] + 1;
+
+                  if (last.typeAnnotation) {
+                    // Section | from "<id>(<...params>:<type>|)  |:<type>"
+                    section[0] = last.typeAnnotation.range[1] + 1;
+                  }
+                }
+              }
+
+              // Text _ from "<id>__(<...params>:<type>_)____:<type>"
+              const text = context.sourceCode.getText(node).substring(
+                section[0] - node.range[0],
+                section[1] - node.range[0]
+              )
+
+              // Section | from "<id>(<...params>)|  |:<type>"
+              const index = text.search(/\)/) + 1;
+
+              section[0] += index;
+
+              if (rangeDistance(section) !== spaceBeforeColon) {
+                context.report({
+                  message: `Wrong colon spacing. Expected ${spaceBeforeColon} space before colon.`,
+                  range: rangePadding(section),
+                  fix(fixer) : Deno.lint.Fix {
+                    return fixer.replaceTextRange(section, ' ');
+                  }
+                });
+              }
+            }
+          },
           FunctionDeclaration(node) : void {
             if (node.returnType && node.id) {
               // Section | from "<id>| (<...params>) |:<type>"
@@ -24,18 +70,18 @@ const colonSpacing : Deno.lint.Plugin = {
                   // Section | from "<id>(<...params>)|  |:<type>"
                   section[0] = last.typeAnnotation.range[1] + 1;
                 }
-              } else {
-                // Text _ from "<id>__(<...params>)____:<type>"
-                const text = context.sourceCode.getText(node).substring(
-                  section[0] - node.range[0],
-                  section[1] - node.range[0]
-                )
-
-                // Section | from "<id>(<...params>)|  |:<type>"
-                const index = text.search(/\)/) + 1;
-
-                section[0] += index;
               }
+
+              // Text _ from "<id>__(<...params>_)____:<type>"
+              const text = context.sourceCode.getText(node).substring(
+                section[0] - node.range[0],
+                section[1] - node.range[0]
+              )
+
+              // Section | from "<id>(<...params>)|  |:<type>"
+              const index = text.search(/\)/) + 1;
+
+              section[0] += index;
 
               if (rangeDistance(section) !== spaceBeforeColon) {
                 context.report({
@@ -113,21 +159,34 @@ const colonSpacing : Deno.lint.Plugin = {
       create(context) : Deno.lint.LintVisitor {
         return {
           TSTypeAnnotation(node) : void {
-            if (['FunctionDeclaration', 'FunctionExpression', 'TSPropertySignature', 'Identifier', 'PropertyDefinition'].includes(node.parent.type)) {
-              const section : Deno.lint.Range = [
-                node.range[0] + 1,
-                node.typeAnnotation.range[0]
-              ]
+            if (node.parent.type !== 'TSFunctionType') {
 
-              if (section[1] - section[0] !== 1) {
+              // Text _ from "<name>___?__:<type>"
+              const section : Deno.lint.Range = [node.range[0] + 1, node.typeAnnotation.range[0]]
+              
+              // Text _ from "<id>__(<...params>:<type>):___(__<type>)"
+              const text = context.sourceCode.getText(node).substring(
+                section[0] - node.range[0],
+                section[1] - node.range[0]
+              )
+
+              // Section | from "<id>(<...params>):|  |(  <type>"
+              const index = text.search(/\(/);
+
+              if (index !== -1) {
+                section[1] = section[0] + index;
+              }
+
+              if (rangeDistance(section) !== spaceAfterColon) {
                 context.report({
-                  message: 'Wrong colon spacing. Expected 1 space after colon.',
+                  message: `Wrong colon spacing. Expected ${spaceAfterColon} space after colon.`,
                   range: rangePadding(section),
                   fix(fixer) : Deno.lint.Fix{
-                    return fixer.replaceTextRange(section, ' ');
-                  }
+                      return fixer.replaceTextRange(section, ' ');
+                    }
                 });
               }
+
             }
           },
           Property(node) : void {
@@ -144,7 +203,7 @@ const colonSpacing : Deno.lint.Plugin = {
               
               if (index !== -1) {
                 context.report({
-                  message: 'Wrong colon spacing. Expected 1 space after colon.',
+                  message: `Wrong colon spacing. Expected ${spaceAfterColon} space after colon.`,
                   range: rangePadding(section),
                   fix(fixer) : Deno.lint.Fix{
                     return fixer.replaceTextRange(section, ' ');
