8e1522d6ce0c3f1dc78d7d7c78c0226d8109b692
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-11-11 10:17:28 +0100

    fix: Colon spacing after

--- src/plugins/colon_spacing.ts
+++ src/plugins/colon_spacing.ts
@@ -124,7 +124,7 @@ const colonSpacing : Deno.lint.Plugin = {
             }
           },
           Identifier(node) : void {
-            if (node.typeAnnotation) {
+            if (node.typeAnnotation && node.parent.type !== 'TSIndexSignature') {
               // Text _ from "<name>___?__:<type>"
               const section : Deno.lint.Range = [node.range[1], node.typeAnnotation.range[0]]
 
